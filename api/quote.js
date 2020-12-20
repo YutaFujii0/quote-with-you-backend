9081e0d9b98f084e2582f99c7575c45248b2187c
YutaFujii0 <y.f.mar2@gmail.com>
2020-12-20 23:41:56 +0900

    Implement quote api with dynamoDB

--- api/quote.js
+++ api/quote.js
@@ -1,14 +1,36 @@
 'use strict';
 
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 module.exports.get = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+  var params = {
+    TableName: process.env.QUOTE_TABLE,
+    ProjectionExpression: "id, quote, author"
   };
 
-  callback(null, response);
+  console.log("Scanning Quote table.");
+  const onScan = (err, data) => {
+
+      if (err) {
+          console.log('Scan failed to load data. Error JSON:', JSON.stringify(err, null, 2));
+          callback(err);
+      } else {
+          console.log("Scan succeeded.");
+          return callback(null, {
+              statusCode: 200,
+              body: JSON.stringify({
+                  quote: data.Items
+              })
+          });
+      }
+
+  };
+
+  dynamoDb.scan(params, onScan);
 
 };
